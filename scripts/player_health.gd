b59c27e348f9607e6c66dd200f6ab6b4e9e41c7a
mrcd_noah <mrcd.noah@gmail.com>
2025-12-26 13:18:59 +0100

    Changement healthbar

--- scripts/player_health.gd
+++ scripts/player_health.gd
@@ -3,6 +3,14 @@ extends Control
 class_name PlayerHealthUI
 
 @onready var health_label : Label = $health
+@onready var vie1 : TextureRect = $HBoxContainer/TextureRect
+@onready var vie2 : TextureRect = $HBoxContainer/TextureRect2
+@onready var vie3 : TextureRect = $HBoxContainer/TextureRect3
 
 func update(health) -> void:
-	health_label.text = "x" + str(health)
+	if health == 2:
+		vie3.visible = false
+	if health == 1:
+		vie2.visible = false
+	if health == 0:
+		vie1.visible = false
